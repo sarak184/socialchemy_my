d99a37dfcba7b381095688e1d8ea8c596e56aa15
sarak184 <31420592+sarak184@users.noreply.github.com>
2019-03-19 18:48:32 +0530

    Add files via upload

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -225,7 +225,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
@@ -253,7 +253,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+  config.omniauth :google_oauth2, '343180924606-qfs665ukvpb8nlfih80gjhkal4ffgmtg.apps.googleusercontent.com', '7qmPSy-69AKu3zohGDkCJhKR', {}
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :posts, only: [:create, :edit, :update]
+  devise_for :users , controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  resources :posts, only: [:create, :edit, :update, :index]
 
   resources :comments, only: [:create]
-
+   get "likes/toggle", as: "like_toggle"
   get "users/my_profile"
   patch "users/update" =>"users#update", as: "user"
   delete "posts/:id" => "posts#destroy", as: "post_destroy"
